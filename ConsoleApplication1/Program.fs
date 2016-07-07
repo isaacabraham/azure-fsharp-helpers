750f353438da7c69fdc62935d30faeda2c42eac1
Isaac Abraham <isaac.abraham@gmail.com>
2016-07-07 13:28:39 +0200

    Allow adding connection strings from environment that don't exist in configuration.

---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.Configuration
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    Environment.SetEnvironmentVariable("SQLCONNSTR_TESTCONN", "TestConnectionString")
+    Environment.SetEnvironmentVariable("APPSETTING_TESTAPP", "TestAppSetting")
+    
+    Azure.applyAzureEnvironmentToConfigurationManager()
+    
+    let connectionString = ConfigurationManager.ConnectionStrings.["TESTCONN"]
+    let appSetting = ConfigurationManager.AppSettings.["TESTAPP"]
+    
+    0 // return an integer exit code
--- src/configuration.fs
+++ src/configuration.fs
@@ -5,7 +5,7 @@ open System.Configuration
 open System.Reflection
 
 module private AzureConfiguration =
-    let (|AppSetting|_|) (key:string,value:string) =
+    let (|AppSetting|_|) (key:string, value:string) =
         let token = "APPSETTING_"
         if key.StartsWith token then Some (key.Substring token.Length, value)
         else None
@@ -30,19 +30,33 @@ module private AzureConfiguration =
             while enumerator.MoveNext() do
                 yield (string enumerator.Key, string enumerator.Value) }
 
+    let private allowAddingNewConnectionStrings() =
+        typeof<ConfigurationElementCollection>
+            .GetField("bReadOnly", BindingFlags.Instance ||| BindingFlags.NonPublic)
+            .SetValue(ConfigurationManager.ConnectionStrings, false)
+
+    let private makeConnectionStringWriteable (connectionStringSettings:ConnectionStringSettings) =
+        typeof<ConfigurationElement>
+            .GetField("_bReadOnly", BindingFlags.Instance ||| BindingFlags.NonPublic)
+            .SetValue(connectionStringSettings, value = false)               
+
     let applyToConfigurationManager settings =
+        allowAddingNewConnectionStrings()
         for setting in settings do
             match setting with
             | AppSetting(key, value) -> ConfigurationManager.AppSettings.[key] <- value
             | DetailedConnectionString (name, connection, provider) ->
-                let setting = ConfigurationManager.ConnectionStrings.[name]
-                if setting <> null then                
-                    typeof<ConfigurationElement>
-                        .GetField("_bReadOnly", BindingFlags.Instance ||| BindingFlags.NonPublic)
-                        .SetValue(setting, value = false)
-                
+                match ConfigurationManager.ConnectionStrings.[name] |> Option.ofObj with
+                | None ->
+                    let connectionStringSettings =
+                        match provider with
+                        | Some provider -> ConnectionStringSettings(name, connection, provider)
+                        | None -> ConnectionStringSettings(name, connection)
+                    ConfigurationManager.ConnectionStrings.Add connectionStringSettings
+                | Some setting ->
+                    makeConnectionStringWriteable setting
                     setting.ConnectionString <- connection                
-                    provider |> Option.iter(fun provider -> setting.ProviderName <- provider)    
+                    provider |> Option.iter(fun provider -> setting.ProviderName <- provider)
             | _ -> ()
 
 /// Applies Azure Application Settings stored as Environment variables into the Configuration Manager.
